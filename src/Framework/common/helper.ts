da6ac9f1f3063d5499b63b839776e19e41b2b628
Dmitry Serbin <dmitry.serbin@iag.co.nz>
2022-07-01 18:03:30 +1200

    Logger

--- src/Framework/common/helper.ts
+++ src/Framework/common/helper.ts
@@ -14,7 +14,11 @@ export class Helper implements IHelper {
 
     public async wait(min: number, max: number): Promise<void> {
 
+        const debug = this.debugLogger.extend(this.wait.name);
+
         const delay: number = Math.floor(Math.random() * (max - min + 1) + min);
+
+        debug(`Waiting <${delay}> milliseconds`);
 
         return new Promise((resolve) => setTimeout(resolve, delay));
 
